2773b47296589a193db1c7c1b39dad2812dbcfb6
Camilo Villarroel Perez <camiloed92@hotmail.com>
2019-06-04 12:45:03 -0400

    Item marcar completado agregado

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   get '/todos', to: 'todos#index'
   root 'todos#index'
   get 'todos/new'
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
   get '/todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
   patch '/todos/:id', to: 'todos#update'
   delete '/todos/:id', to: 'todos#destroy' 
+  post '/todos/:id/complete', to: 'todos#complete', as: 'complete_todo'
+  
 end
