3a328d56c8ed8bc53d6b3964d0b19f51d5f3f611
Camilo Villarroel Perez <camiloed92@hotmail.com>
2019-06-04 12:22:12 -0400

    Item Update agregado

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'todos/new'
   post '/todos', to: 'todos#create'
   get '/todos/:id', to: 'todos#show', as:'todo'
-  get '/todos/:id/edit', to: 'todos#edit', as: 'edit_todo' 
+  get '/todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  patch '/todos/:id', to: 'todos#update' 
 end
